c9a2cfdffc795304b70f536a67a67aff92b53c61
khanhnguyentuann <khanh.nguyentuann@gmail.com>
2023-10-11 23:26:59 +0700

    update title search

--- routes/user/search.js
+++ routes/user/search.js
@@ -3,53 +3,61 @@ const router = express.Router();
 const config = require('../../knexfile').development;
 const knex = require('knex')(config);
 
-router.get('/searchByTag', async (req, res) => {
+// Search recipes by tags
+const searchByTag = async (req, res) => {
     const tagString = req.query.tag;
-    const tags = tagString.split(',').map(tag => tag.trim()); // Tách chuỗi thành một mảng các tag
-
+    const tags = tagString.split(',').map(tag => tag.trim());
     try {
         const recipes = await knex('recipes')
             .join('recipe_tags', 'recipes.id', '=', 'recipe_tags.recipe_id')
             .join('tags', 'tags.id', '=', 'recipe_tags.tag_id')
-            .whereIn('tags.tag_name', tags)  // Sử dụng .whereIn() thay cho .where()
-            .distinct('recipes.id', 'recipes.name')  // Sử dụng .distinct để loại bỏ các bản sao
+            .whereIn('tags.tag_name', tags)
+            .distinct('recipes.id', 'recipes.name')
             .select('recipes.*');
-
         res.status(200).json({ recipes });
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
-});
+};
 
-router.get('/searchByTitle', async (req, res) => {
+// Search recipes by title
+const searchByTitle = async (req, res) => {
     const title = req.query.title;
-
-    // Tách từ khóa tìm kiếm thành các từ riêng lẻ và chuyển thành chữ thường
     const keywords = title.toLowerCase().split(' ').map(word => `%${word}%`);
-
     try {
-        // Tạo điều kiện tìm kiếm từ các từ khóa
         const query = knex('recipes').select('id', 'name', 'preparationTime', 'difficulty');
-        keywords.forEach(keyword => {
-            query.andWhere(knex.raw('LOWER(name) LIKE ?', [keyword]));
-        });
-
+        if (keywords.length > 0) {
+            // xây dựng một hoặc nhiều điều kiện tìm kiếm trả về các bài viết có chứa ít nhất một từ khóa.
+            query.where(function () {
+                keywords.forEach((keyword, index) => {
+                    if (index === 0) {
+                        this.where(knex.raw('LOWER(name) LIKE ?', [keyword]));
+                    } else {
+                        this.orWhere(knex.raw('LOWER(name) LIKE ?', [keyword]));
+                    }
+                });
+            });
+        }
         const recipes = await query;
-
         res.status(200).json({ recipes });
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
-});
+};
 
-// Endpoint to get all tags
-router.get('/getAllTags', async (req, res) => {
+// Get all tags
+const getAllTags = async (req, res) => {
     try {
         const tags = await knex('tags').select('id', 'tag_name');
         res.status(200).json({ tags });
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
-});
+};
+
+// Define routes
+router.get('/searchByTag', searchByTag);
+router.get('/searchByTitle', searchByTitle);
+router.get('/getAllTags', getAllTags);
 
 module.exports = router;
