0ddbc108f801438fd591394b2c93cf3f8eafe08d
khanhnguyentuann <khanh.nguyentuann@gmail.com>
2023-11-08 20:10:18 +0700

    update search

--- routes/user/search.js
+++ routes/user/search.js
@@ -3,20 +3,64 @@ const router = express.Router();
 const config = require('../../knexfile').development;
 const knex = require('knex')(config);
 
+// Get all tags
+const getAllTags = async (req, res) => {
+    try {
+        const tags = await knex('tags').select('id', 'tag_name');
+        res.status(200).json({ tags });
+    } catch (error) {
+        handleRouteError(res, error);
+    }
+};
+
+// Get all igredients
+const getAllIngredients = async (req, res) => {
+    try {
+        const ingredients = await knex('ingredients').select('*');
+        res.status(200).json({ ingredients });
+    } catch (error) {
+        handleRouteError(res, error);
+    }
+};
+
+// Function to create a basic recipes query
+const createBaseRecipesQuery = () => {
+    return knex('recipes').select('recipes.id', 'recipes.name', 'recipes.cookingTime', 'recipes.servingFor').distinct('recipes.id');
+};
+
+// Helper function to add the first image to recipes
+const addFirstImageToRecipes = async (recipesQuery) => {
+    const recipesWithImages = await recipesQuery
+        .leftJoin('recipe_images', function () {
+            this.on('recipes.id', '=', 'recipe_images.recipe_id')
+                .onIn('recipe_images.id', knex.raw('SELECT min(id) FROM recipe_images GROUP BY recipe_id'));
+        })
+        .select('recipe_images.image_url as firstImage');
+    return recipesWithImages;
+};
+
+// Error handling helper function
+const handleRouteError = (res, error) => {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+};
+
 // Search recipes by tags
 const searchByTag = async (req, res) => {
     const tagString = req.query.tag;
     const tags = tagString.split(',').map(tag => tag.trim());
     try {
-        const recipes = await knex('recipes')
-            .join('recipe_tags', 'recipes.id', '=', 'recipe_tags.recipe_id')
-            .join('tags', 'tags.id', '=', 'recipe_tags.tag_id')
-            .whereIn('tags.tag_name', tags)
-            .distinct('recipes.id', 'recipes.name')
-            .select('recipes.*');
+        const baseQuery = createBaseRecipesQuery();
+        const recipes = await addFirstImageToRecipes(
+            baseQuery
+                .join('recipe_tags', 'recipes.id', '=', 'recipe_tags.recipe_id')
+                .join('tags', 'tags.id', '=', 'recipe_tags.tag_id')
+                .whereIn('tags.tag_name', tags)
+                .orderBy('recipes.id', 'asc')
+        );
         res.status(200).json({ recipes });
     } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+        handleRouteError(res, error);
     }
 };
 
@@ -25,60 +69,36 @@ const searchByTitle = async (req, res) => {
     const title = req.query.title;
     const keywords = title.toLowerCase().split(' ').map(word => `%${word}%`);
     try {
-        const query = knex('recipes').select('id', 'name', 'preparationTime', 'difficulty');
-        if (keywords.length > 0) {
-            // xây dựng một hoặc nhiều điều kiện tìm kiếm trả về các bài viết có chứa ít nhất một từ khóa.
-            query.where(function () {
-                keywords.forEach((keyword, index) => {
-                    if (index === 0) {
-                        this.where(knex.raw('LOWER(name) LIKE ?', [keyword]));
-                    } else {
-                        this.orWhere(knex.raw('LOWER(name) LIKE ?', [keyword]));
-                    }
-                });
+        const baseQuery = createBaseRecipesQuery()
+            .where(function () {
+                this.where(knex.raw('LOWER(recipes.name) LIKE ?', [keywords[0]]));
+                for (let i = 1; i < keywords.length; i++) {
+                    this.orWhere(knex.raw('LOWER(recipes.name) LIKE ?', [keywords[i]]));
+                }
             });
-        }
-        const recipes = await query;
+
+        const recipes = await addFirstImageToRecipes(baseQuery);
         res.status(200).json({ recipes });
     } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+        handleRouteError(res, error);
     }
 };
 
-// Get all tags
-const getAllTags = async (req, res) => {
-    try {
-        const tags = await knex('tags').select('id', 'tag_name');
-        res.status(200).json({ tags });
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-};
-
-const getAllIngredients = async (req, res) => {
-    try {
-        const ingredients = await knex('ingredients').select('*');
-        res.status(200).json({ ingredients });
-    } catch (error) {
-        console.error("Lỗi khi lấy danh sách nguyên liệu:", error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-};
-
+// Search recipes by ingredients
 const searchByIngredients = async (req, res) => {
     const ingredientString = req.query.ingredients;
     const ingredients = ingredientString.split(',').map(ingredient => ingredient.trim());
 
     try {
-        const recipes = await knex('recipes')
+        const baseQuery = createBaseRecipesQuery()
             .join('recipe_ingredients', 'recipes.id', '=', 'recipe_ingredients.recipe_id')
             .join('ingredients', 'ingredients.id', '=', 'recipe_ingredients.ingredient_id')
-            .whereIn('ingredients.name', ingredients)
-            .distinct('recipes.id', 'recipes.name')
-            .select('recipes.*');
+            .whereIn('ingredients.name', ingredients);
+
+        const recipes = await addFirstImageToRecipes(baseQuery);
         res.status(200).json({ recipes });
     } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+        handleRouteError(res, error);
     }
 };
 
